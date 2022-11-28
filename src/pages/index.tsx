703ea01e6e915e4a3855d06ea583306f823d084d
{cocoide} <{cocoide.com@gmail.com}>
2022-11-29 03:31:35 +0900

    setup tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter</title>
+        <link rel="icon" href="/twitter.svg" />
+      </Head>
+
+
+      <main>
+      </main>
+    </div>
+  )
+}

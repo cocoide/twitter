4dce7dfe5da508eba81e1050c9a10347eb1c865f
{cocoide} <{cocoide.com@gmail.com}>
2022-11-30 14:00:08 +0900

    Add input section of feed component

--- src/components/Organisms/FeedForm.tsx
+++ src/components/Organisms/FeedForm.tsx
@@ -5,20 +5,25 @@ const FeedForm = () => {
     return (
         <div className="flex flex-col border-b border-gray-200 p-3 space-x-3">
 
-
-            <div className="flex flex-row">
+            <div className="divide-y divide-gray-200">
+                <div className="flex">
                 <TwitterIcon />
-                <textarea rows={2} className="w-full" placeholder="What's happing?"></textarea>
+                    <textarea className="w-full border-none focus:ring-0"
+                        rows={2} placeholder="What's happing?"></textarea>
             </div>
 
 
-            <div className="flex flex-row">
-                <PhotoIcon className="w-10"></PhotoIcon>
-                <FaceSmileIcon className="w-10"></FaceSmileIcon>
-                <button className="">tweet</button>
+                <div className="flex justify-between items-center pt-2">
+                    <div className="flex">
+                        <PhotoIcon className="w-10 hoverEffect p-2 text-sky-500"></PhotoIcon>
+                        <FaceSmileIcon className="w-10 hoverEffect p-2 text-sky-500"></FaceSmileIcon>
+                    </div>
+                    <button className="bg-[#1DA1F2] rounded-full font-bold py-1.5 px-4 text-white shadow-md
+                    hover:brightness-95 disabled:opacity-50"
+                    >Tweet</button>
             </div>
 
-
+            </div>
         </div>
     )
 }
